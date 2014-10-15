c46f2f826e4fd707ab7ede3af167f216687606f8
mshugrue <melodyshugrue@gmail.com>
2014-10-15 16:19:05 -0500

    Homework day 1

---
+++ Homework-1.py
@@ -0,0 +1,15 @@
+import math
+
+first = "Melody"
+last = "Shugrue"
+age = 15
+print first , last , age 
+
+sideA = 12.55
+sideB = 17.85
+sideC = math.sqrt( (sideA * sideA) + (sideB * sideB) )
+print "A triangle with sides A:" , sideA , "and B:" , sideB , "has a side C equal to:" , sideC
+
+operand1 = 95
+operand2 = 64.5
+print "95 + 64.5 =" , operand1 + operand2 , "\n95 - 64.5 =" , operand1 - operand2 , "\n95 * 64.5 =" , operand1 * operand2 , "\n95 / 64.5 =" , operand1 / operand2 , "\n95 % 64.5 =" , operand1 % operand2  
